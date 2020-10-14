dbd8b31563409804ca78459d1c858fbd589e76d8
Arthur Saveliev <arthur.saveliev@gmail.com>
2020-10-14 20:37:34 +0200

    adds sample app functionality

--- app/src/main/java/com/manylogs/android/MainActivity.kt
+++ app/src/main/java/com/manylogs/android/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.manylogs.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private val API_URL = "https://jsonplaceholder.typicode.com"
+    private val pathPosts = "$API_URL/posts"
+    private val pathUsers = "$API_URL/users"
+
+    private val client: OkHttpClient = createClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnPosts)?.setOnClickListener { getPosts() }
+        findViewById<Button>(R.id.btnUsers)?.setOnClickListener { getUsers() }
+    }
+
+    private fun createClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(logging)
+        builder.connectTimeout(30, TimeUnit.SECONDS)
+        builder.readTimeout(30, TimeUnit.SECONDS)
+        builder.writeTimeout(30, TimeUnit.SECONDS)
+        return builder.build()
+    }
+
+    private fun getPosts() {
+        val request: Request = Request.Builder()
+            .url(pathPosts)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+            }
+        })
+    }
+    private fun getUsers() {
+        val request: Request = Request.Builder()
+            .url(pathUsers)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+            }
+        })
     }
 }
