78e4adfdea97a95f6fc89f607012194ce12bb012
Arthur Saveliev <arthur.saveliev@gmail.com>
2020-10-17 20:06:57 +0200

    mian app updates

--- app/src/main/java/com/manylogs/android/MainActivity.kt
+++ app/src/main/java/com/manylogs/android/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.manylogs.logging.ManyLogsClient
 import com.manylogs.logging.ManyLogsInterceptor
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private val API_URL = "https://jsonplaceholder.typicode.com"
     private val pathPosts = "$API_URL/posts"
     private val pathUsers = "$API_URL/users"
+    private val MEDIA_TYPE = "application/json; charset=utf-8".toMediaType()
 
     private lateinit var client: OkHttpClient
     private lateinit var manyLogsClient: ManyLogsClient
@@ -26,8 +29,10 @@ class MainActivity : AppCompatActivity() {
 
         initClients()
 
+
         findViewById<Button>(R.id.btnPosts)?.setOnClickListener { getPosts() }
         findViewById<Button>(R.id.btnUsers)?.setOnClickListener { getUsers() }
+        findViewById<Button>(R.id.btnSendPost)?.setOnClickListener { sendPost() }
     }
 
     private fun initClients() {
@@ -54,6 +59,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun getPosts() {
         val request: Request = Request.Builder()
+            .addHeader("text-header", "Test Header Value")
             .url(pathPosts)
             .build()
         client.newCall(request).enqueue(object : Callback {
@@ -66,7 +72,30 @@ class MainActivity : AppCompatActivity() {
     }
     private fun getUsers() {
         val request: Request = Request.Builder()
+            .addHeader("text-header", "Test Header Value")
             .url(pathUsers)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+            }
+        })
+    }
+
+    private fun sendPost() {
+
+        val json = JSONObject()
+        json.put("userId", "1")
+        json.put("title", "Test post")
+        json.put("body", "This is a test post body")
+
+        val body = RequestBody.create(MEDIA_TYPE, json.toString())
+
+        val request: Request = Request.Builder()
+            .url(pathPosts)
+            .post(body)
             .build()
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
